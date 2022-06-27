76fc8dff81e557a138a172124d6fa936a10ee78b
DZM77 <dimitris.bjorlingh@outlook.com>
2022-06-27 11:57:24 +0200

    XMLHttpRequest

--- Lektion3/XMLHttp/script.js
+++ Lektion3/XMLHttp/script.js
@@ -6,11 +6,46 @@
 // DELETE - Tar bort ett objekt
 
 
+const getRequest = (url, cb) => {
+    
+    const http = new XMLHttpRequest();
+
+    http.addEventListener('readystatechange', () => {
+       if(http.readyState === 4 && http.status === 200){
+            const data = JSON.parse(http.responseText);
+            // console.log(data);
+            cb(undefined, data);
+       }
+       else if(http.readyState === 4){
+            cb('Server error', undefined);
+       }
+    })
 
 
 
 
+    http.open('GET', url)
+    http.send();
+}
 
+//Callback hell!
+console.log(1);
+console.log(2);
+console.log(3);
+
+getRequest('user.json', (err, data) => {
+    console.log(err, data);
+        getRequest('todos.json', (err, data) => {
+            console.log(err, data); 
+                getRequest('todos2.json', (err, data) => {
+                    console.log(err, data) 
+                })
+            })
+} );
+
+
+console.log(4);
+console.log(5);
 
 
     
