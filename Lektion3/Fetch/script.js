07b4c75b87e79342b094d03d3c9db3c6e9ff6749
DZM77 <dimitris.bjorlingh@outlook.com>
2022-06-27 14:51:28 +0200

    Async await

--- Lektion3/Fetch/script.js
+++ Lektion3/Fetch/script.js
@@ -12,13 +12,13 @@ let textoutput = document.querySelector('#text-output');
 // })
 
 
-btn1.addEventListener('click', function(){
+btn1.addEventListener('click', function () {
     fetch('text.txt')
-        .then(function(res){
+        .then(function (res) {
             // console.log(res);
             return res.text();
         })
-        .then(function(data) {
+        .then(function (data) {
             console.log(data);
             textoutput.innerHTML = `<p>${data}</p>`
         })
@@ -35,8 +35,8 @@ const getJson = () => {
 
             jsonoutput.innerHTML = "";
             data.forEach(post => {
-                jsonoutput.innerHTML += 
-                `
+                jsonoutput.innerHTML +=
+                    `
                 <div class="card">
                 <div class="card-body">
                     <h3 class="card-title">${post.title} </h3>
@@ -57,18 +57,18 @@ const gitOutput = document.querySelector('#git-output');
 
 const getGit = () => {
 
-        fetch('https://api.github.com/users/NET22B/repos', {
-            method : 'GET',
-            headers : {
-                'Accept' : 'application/json'
-            }
-        })
+    fetch('https://api.github.com/users/NET22B/repos', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
         .then(res => res.json())
         .then(data => {
             gitOutput.innerHTML = '';
 
             data.forEach(repo => {
-                gitOutput.innerHTML += 
+                gitOutput.innerHTML +=
                     `
             <div class="card">
             <div class="card-body">
@@ -89,7 +89,66 @@ const getGit = () => {
 gitBtn.addEventListener('click', getGit);
 
 
+const getData = async (url) => {
 
+   const res = await fetch(url)
+//    console.log(res);
+
+    if(res.status !== 200){
+        throw new Error('Error');
+    }
+
+   const data = await res.json();
+
+//    console.log(data);
+
+   return data;
+}
+
+
+// const test = getData('json.json');
+// console.log(test);
+
+getData('json.json')
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+
+
+
+
+
+const gitAsyncBtn = document.querySelector('#btn4');
+const gitAsyncOutput = document.querySelector('#gitasync-output');
+
+const getGitAsync = async () => {
+
+    const res = await fetch('https://api.github.com/users/NET22B/repos', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+    const data = await res.json();
+
+    gitAsyncOutput.innerHTML = '';
+    data.forEach(repo => {
+            gitAsyncOutput.innerHTML +=
+            `
+            <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${repo.name}</h5>
+              <p class="card-text">This repository have been forked ${repo.forks_count} times</p>
+              <a href="${repo.html_url}" class="card-link">${repo.name}</a>
+              <a href="${repo.owner.html_url}" class="card-link">Organisation</a>
+            </div>
+            </div>
+
+            `
+        })
+
+}
+
+gitAsyncBtn.addEventListener('click', getGitAsync);
 
 
 
